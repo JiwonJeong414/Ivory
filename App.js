eeb0b2fbade0def4ea2e0cb0815ae707857259e8
jiwonjeong414 <jiwonjeong414@gmail.com>
2023-02-16 22:13:47 -0800

    Initial Commit

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Image
+        style={styles.graphics}
+        source={require("./assets/pictures/home-graphics.png")}
+      />
     </View>
   );
 }
@@ -13,8 +17,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#f9efe0",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  graphics: {
+    width: "100%",
+    height: "30%",
   },
 });
---
+++ color.js
@@ -0,0 +1,36 @@
+:root {
+	--ion-color-primary: #f9efe0;
+	--ion-color-primary-rgb: 249,239,224;
+	--ion-color-primary-contrast: #000000;
+	--ion-color-primary-contrast-rgb: 0,0,0;
+	--ion-color-primary-shade: #dbd2c5;
+	--ion-color-primary-tint: #faf1e3;
+
+	--ion-color-secondary: #e1bea3;
+	--ion-color-secondary-rgb: 225,190,163;
+	--ion-color-secondary-contrast: #000000;
+	--ion-color-secondary-contrast-rgb: 0,0,0;
+	--ion-color-secondary-shade: #c6a78f;
+	--ion-color-secondary-tint: #e4c5ac;
+
+	--ion-color-tertiary: #ebb871;
+	--ion-color-tertiary-rgb: 235,184,113;
+	--ion-color-tertiary-contrast: #000000;
+	--ion-color-tertiary-contrast-rgb: 0,0,0;
+	--ion-color-tertiary-shade: #cfa263;
+	--ion-color-tertiary-tint: #edbf7f;
+
+	--ion-color-success: #06ae7e;
+	--ion-color-success-rgb: 6,174,126;
+	--ion-color-success-contrast: #ffffff;
+	--ion-color-success-contrast-rgb: 255,255,255;
+	--ion-color-success-shade: #05996f;
+	--ion-color-success-tint: #1fb68b;
+
+	--ion-color-warning: #ee7377;
+	--ion-color-warning-rgb: 238,115,119;
+	--ion-color-warning-contrast: #000000;
+	--ion-color-warning-contrast-rgb: 0,0,0;
+	--ion-color-warning-shade: #d16569;
+	--ion-color-warning-tint: #f08185;
+}

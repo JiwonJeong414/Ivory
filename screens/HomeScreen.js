59972f4cdc1d64fafcdb8d25b5b8e77c289d6be1
jiwonjeong414 <jiwonjeong414@gmail.com>
2023-02-19 23:42:52 -0800

    Calculus Canvas Request Api Stage 1

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -3,10 +3,47 @@ import { StyleSheet, View, Text, Button } from "react-native";
 import firebase from "firebase/app";
 import "firebase/auth";
 import { useNavigation } from "@react-navigation/native";
+import axios from "axios";
+
+const apiUrl = "https://iusd.instructure.com/api/v1";
+const token =
+  "3007~pk06pga4ouulN1dGIjmnFdCwfo9DpAJNxTTqlAFpMCKpzHYZALQNobl512KgOQhB";
+axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+
+async function getAssignments() {
+  try {
+    const courseId = await getCourseId("Calculus AB-B (AP) - Chang, P");
+    const response = await axios.get(
+      `${apiUrl}/courses/${courseId}/announcements`
+    );
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function getCourseId(courseName) {
+  try {
+    console.log("course: ");
+    const response = await axios.get(`${apiUrl}/courses`);
+    const course = response.data.find((c) => c.name === courseName);
+    console.log(course);
+    return course.id;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// Example usage
+async function handleClick() {
+  const assignments = await getAssignments();
+  console.log(assignments);
+}
 
 export default function HomeScreen() {
   const navigation = useNavigation();
   const [user, setUser] = useState(null);
+  const [assignments, setAssignments] = useState([]);
 
   useEffect(() => {
     const currentUser = firebase.auth().currentUser;
@@ -32,6 +69,7 @@ export default function HomeScreen() {
         </View>
       )}
       <Button title="Sign Out" onPress={handleSignOut} />
+      <Button title="Get Assignments" onPress={handleClick} />
     </View>
   );
 }
